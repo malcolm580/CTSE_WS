ae2b54279fc8a9256ac1d8798da630d51dcd3575
malcolm580 <youmalcolm580@gmail.com>
2017-10-25 22:49:12 +0800

    ....

--- src/Assignment/RiceCreateCommand.java
+++ src/Assignment/RiceCreateCommand.java
@@ -10,42 +10,44 @@ public class RiceCreateCommand extends CreateCommand {
     @Override
     public FoodItem factoryMethod() {
 
-        System.out.println("Enter ItemID:");
-        line = null;
-        try {
-            line = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        int id = Integer.parseInt(line);
+        // replace by line spliter
 
-
-        System.out.println("Enter name:");
-        line = null;
-        try {
-            line = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String name = line;
-
-        System.out.println("Enter balance:");
-        line = null;
-        try {
-            line = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        int balance = Integer.parseInt(line);
-
-        System.out.println("Enter Type:");
-        line = null;
-        try {
-            line = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String type = line;
+//        System.out.println("Enter ItemID:");
+//        line = null;
+//        try {
+//            line = br.readLine();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        int id = Integer.parseInt(line);
+//
+//
+//        System.out.println("Enter name:");
+//        line = null;
+//        try {
+//            line = br.readLine();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        String name = line;
+//
+//        System.out.println("Enter balance:");
+//        line = null;
+//        try {
+//            line = br.readLine();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        int balance = Integer.parseInt(line);
+//
+//        System.out.println("Enter Type:");
+//        line = null;
+//        try {
+//            line = br.readLine();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        String type = line;
 
         System.out.println("New InstantNoodle: id("+id+"),name("+name+"),balance("+balance+"),type:("+type+")");
 
